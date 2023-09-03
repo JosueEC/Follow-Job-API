771bde1d6c75234785732146dea7657f62528eaf
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-03 11:37:22 -0600

    feat(src): 🔨 Add user resource to the project with it's module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
