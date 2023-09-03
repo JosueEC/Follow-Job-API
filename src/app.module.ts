62bd977187e9d65cb7988dc7967f6838b309d179
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-03 11:25:10 -0600

    build(app.module): 🔧 Add connection configuration with postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'followjob_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
