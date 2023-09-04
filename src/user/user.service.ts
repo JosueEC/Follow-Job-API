546c6f8e89acbcb1543d18130ee22d0f2760815a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-04 11:51:08 -0600

    feat(user.service): 🔨 Add exists and create service functions

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  public async exists(email: string): Promise<boolean> {
+    const userFound = await this.userRepository.findOneBy({ email });
+
+    return userFound ? true : false;
+  }
+
+  public async create(user: CreateUserDto): Promise<User> {
+    const userExists = await this.exists(user.email);
+
+    if (userExists) {
+      throw new ConflictException(`The e-mail ${user.email} is already in use`);
+    }
+
+    const userCreated = this.userRepository.create(user);
+    return await this.userRepository.save(userCreated);
+  }
+}
