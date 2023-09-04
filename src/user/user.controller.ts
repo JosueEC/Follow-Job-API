77f0aa564915bdb02eaa81664b5a0ff92f3eeeb0
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-04 12:09:41 -0600

    feat(user): 🔨 Add createUser route in the controller and add the user modules to the user.module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
 
 @Controller('/v1/user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  public async createUser(@Body() user: CreateUserDto): Promise<User> {
+    return this.userService.create(user);
+  }
+}
