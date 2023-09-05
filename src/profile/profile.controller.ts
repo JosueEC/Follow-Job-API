1e30a5a254264dc9beda7405b76c31c035df5c00
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-04 19:40:45 -0600

    feat(profile): 🔨 Add feature createProfile with it's route controller, service function and create-profile.dto

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Body, Post } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { ProfileService } from './profile.service';
 
 @Controller('/v1/profile')
-export class ProfileController {}
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Post(':id')
+  public async createProfile(
+    @Param('id') id: string,
+    @Body() profile: CreateProfileDto,
+  ) {
+    return await this.profileService.create(id, profile);
+  }
+}
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { Profile } from './entities/profile.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(
+    @InjectRepository(Profile)
+    private readonly profileRepository: Repository<Profile>,
+    private readonly userService: UserService,
+  ) {}
+
+  public async create(id: string, profile: CreateProfileDto): Promise<Profile> {
+    const userFound = await this.userService.findOne(id);
+
+    const profileCreated = this.profileRepository.create(profile);
+    const profileSaved = await this.profileRepository.save(profileCreated);
+
+    userFound.profile = profileSaved;
+    await this.userService.save(userFound);
+
+    return profileSaved;
+  }
+}
