a60fe5f1ce219b6f5f282ef68a96a8cf5cf19648
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-04 19:42:38 -0600

    feat(app.module): 🔨 Add ProfileProvider, ProfileController, UserService import and Profile entity

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from './entities/profile.entity';
+import { ProfileController } from './profile.controller';
+import { ProfileService } from './profile.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Profile]), UserModule],
+  controllers: [ProfileController],
+  providers: [ProfileService],
   exports: [],
 })
 export class ProfileModule {}
