2e1ef656b8a312cb49fdb2695d01b6e43288179b
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-06 13:43:20 -0600

    feat(occupation): 🔨 Add feature createOccupation with it´s route controller and service function

--- src/occupation/occupation.controller.ts
+++ src/occupation/occupation.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { CreateOccupationDto } from './dto/create-occupation.dto';
+import { Occupation } from './entities/occupation.entity';
+import { OccupationService } from './occupation.service';
 
 @Controller('/v1/occupation')
-export class OccupationController {}
+export class OccupationController {
+  constructor(private readonly occupationService: OccupationService) {}
+
+  public async createOccupation(
+    occupationDto: CreateOccupationDto,
+  ): Promise<Occupation> {
+    return await this.occupationService.create(occupationDto);
+  }
+}
--- src/occupation/occupation.service.ts
+++ src/occupation/occupation.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Occupation } from './entities/occupation.entity';
+import { Repository } from 'typeorm';
+import { CreateOccupationDto } from './dto/create-occupation.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class OccupationService {}
+export class OccupationService {
+  constructor(
+    @InjectRepository(Occupation)
+    private readonly occupationRepository: Repository<Occupation>,
+    private readonly userService: UserService,
+  ) {}
+
+  public async create(occupationDto: CreateOccupationDto): Promise<Occupation> {
+    const userFound = await this.userService.findOne(
+      occupationDto.professionalId,
+    );
+
+    if (!userFound) {
+      throw new NotFoundException('User not found :(');
+    }
+
+    const newOccupation = this.occupationRepository.create(occupationDto);
+    return await this.occupationRepository.save(newOccupation);
+  }
+}
