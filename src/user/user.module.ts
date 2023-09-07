744bec4e0173838d356a9c677a24ed91753ecc1a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-06 20:06:32 -0600

    feat(user): 🔨 Add the UserFiltersService to the user.module providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,12 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { IsEmailNotRegisteredConstraint } from './dto/custom-validators/is-email-not-registered';
+import { UserFiltersService } from './userFilters.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, IsEmailNotRegisteredConstraint],
+  providers: [UserService, UserFiltersService, IsEmailNotRegisteredConstraint],
   exports: [UserService],
 })
 export class UserModule {}
