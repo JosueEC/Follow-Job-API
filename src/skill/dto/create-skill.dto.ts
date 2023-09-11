2822012bbb7f4a1b81db35512ac209009f0159cb
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-11 15:44:47 -0600

    fix(skill): 🐛 Fix MinLength property form class-validator in the create-skill.dto

--- src/skill/dto/create-skill.dto.ts
+++ src/skill/dto/create-skill.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 export class CreateSkillDto {
   @IsString()
   @IsNotEmpty()
-  @MinLength(3)
+  @MinLength(1)
   @MaxLength(255)
   name: string;
 }
