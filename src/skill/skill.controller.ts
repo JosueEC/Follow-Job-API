ed948a8ae7633c1ae3286bc2ae31aa8d5011e50f
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-11 15:05:35 -0600

    feat(skill): 🔨 Add feature createSkill with it's route controller and service function

--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { CreateSkillDto } from './dto';
+import { SkillService } from './skill.service';
+import { Skill } from './entities';
 
 @Controller('v1/skill')
-export class SkillController {}
+export class SkillController {
+  constructor(private readonly skillService: SkillService) {}
+  @Post()
+  public async createSkill(@Body() skill: CreateSkillDto): Promise<Skill> {
+    return await this.skillService.create(skill);
+  }
+}
--- src/skill/skill.service.ts
+++ src/skill/skill.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Skill } from './entities';
+import { Repository } from 'typeorm';
+import { CreateSkillDto } from './dto';
 
 @Injectable()
-export class SkillService {}
+export class SkillService {
+  constructor(
+    @InjectRepository(Skill)
+    private readonly skillRepository: Repository<Skill>,
+  ) {}
+
+  public async create(skill: CreateSkillDto): Promise<Skill> {
+    const skillExists = await this.skillRepository.findOneBy({
+      name: skill.name,
+    });
+
+    if (skillExists) {
+      throw new ConflictException(`The skill ${skill.name} already exists`);
+    }
+
+    const newSkill = this.skillRepository.create(skill);
+    return this.skillRepository.save(newSkill);
+  }
+}
