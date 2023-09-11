27823535409de54e63425db246d97e8be8897513
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-11 12:59:36 -0600

    feat(skill): 🔨 Add class-validations to the create-skill.dto into the /dto

--- src/skill/dto/create-skill.dto.ts
+++ src/skill/dto/create-skill.dto.ts
@@ -1,7 +1,20 @@
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { LevelSkill } from '../enums';
 
 export class CreateSkillDto {
-  id: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(255)
   name: string;
+
+  @IsNotEmpty()
+  @IsEnum(LevelSkill)
   level: LevelSkill;
 }
