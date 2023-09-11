3b4dfc0b909d6b450f7413e7e46e8ecbd470eb14
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-11 15:29:34 -0600

    feat(skill): 🔨 Add the update-skill.dto with class-validations and add it to the index.ts into the /dto folder

--- src/skill/dto/index.ts
+++ src/skill/dto/index.ts
@@ -1,1 +1,2 @@
 export { CreateSkillDto } from './create-skill.dto';
+export { UpdateSkillDto } from './update-skill.dto';
--- src/skill/entities/skill.entity.ts
+++ src/skill/entities/skill.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity({ name: 'skill' })
 export class Skill {
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
 
   @Column({ name: 'name', type: 'varchar', length: 255, unique: true })
   name: string;
