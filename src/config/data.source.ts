b1fee8e36c99c27c80271f1890382c2363f32506
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-21 22:41:59 -0600

    feat(config): 🗃️ Add the following configurations:
    - Add the .env file to the ConfigModule
    - Add the DataSourceConfig with the DataSourceOptions that you get from the ConfigService
    - Add the DataSource with the DataSourceConfig

---
+++ src/config/data.source.ts
@@ -0,0 +1,30 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+ConfigModule.forRoot({
+  envFilePath: `.${process.env.NODE_ENV}.env`,
+});
+
+const configService = new ConfigService();
+
+export const DataSourceConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: false,
+  migrationsRun: true,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+// Por ultimo creamos una instancia de DataSource al cual le
+// pasamos nuestro objeto de configuracion con las DataSourceOptions
+// y exportamos estas instancia
+export const AppDS = new DataSource(DataSourceConfig);
