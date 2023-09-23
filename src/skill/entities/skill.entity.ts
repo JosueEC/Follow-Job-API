82b645d07c758d619853469cbc8d62c5ba876428
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-22 22:09:34 -0600

    refactor(skill): ♻️ Change the name Skill to SkillEntity and refeactor this name in all skill files

--- src/skill/entities/skill.entity.ts
+++ src/skill/entities/skill.entity.ts
@@ -1,23 +1,16 @@
-import { Occupation } from '../../occupation/entities/occupation.entity';
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity } from 'typeorm';
+import { ISkill } from '../interfaces/skill.interface';
+import { OccupationEntity } from 'src/occupation/entities/occupation.entity';
 
 @Entity({ name: 'skill' })
-export class Skill {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ name: 'name', type: 'varchar', length: 255, unique: true })
+export class SkillEntity extends BaseEntity implements ISkill {
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 255,
+    unique: true,
+  })
   name: string;
 
-  // Esta es la segunda parte de la relacion n:n. Basicamente es lo
-  // mismo en cada entidad, solo modificando la entidad con la que
-  // se establece la relacion del otro lado. Esta relacion lo que hara
-  // es crear una tabla intermedia entre ambas entidades, la cual
-  // contendra los id's de las relaciones
-
-  // El segundo parametro es para poder realizar consultas de forma
-  // bidireccional, por ende acceder a una skill y devolver la
-  // lista de ocupaciones a la que esta asociada y viceversa
-  @ManyToMany(() => Occupation, (occupation) => occupation.skills)
-  occupations: Occupation[];
+  occupations: OccupationEntity[];
 }
--- src/skill/services/skill.service.ts
+++ src/skill/services/skill.service.ts
@@ -5,22 +5,22 @@ import {
   ServiceUnavailableException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Skill } from '../entities';
+import { SkillEntity } from '../entities';
 import { Repository } from 'typeorm';
 import { CreateSkillDto, UpdateSkillDto } from '../dto';
 
 @Injectable()
 export class SkillService {
   constructor(
-    @InjectRepository(Skill)
-    private readonly skillRepository: Repository<Skill>,
+    @InjectRepository(SkillEntity)
+    private readonly skillRepository: Repository<SkillEntity>,
   ) {}
 
-  public async findAll(): Promise<Skill[]> {
+  public async findAll(): Promise<SkillEntity[]> {
     return await this.skillRepository.find();
   }
 
-  public async create(skill: CreateSkillDto): Promise<Skill> {
+  public async create(skill: CreateSkillDto): Promise<SkillEntity> {
     const skillExists = await this.skillRepository.findOneBy({
       name: skill.name,
     });
@@ -33,19 +33,34 @@ export class SkillService {
     return this.skillRepository.save(newSkill);
   }
 
-  public async update(skill: UpdateSkillDto): Promise<Skill> {
-    const skillExists = await this.skillRepository.findOneBy({ id: skill.id });
+  public async update(skill: UpdateSkillDto): Promise<SkillEntity> {
+    const skillExists = await this.skillRepository
+      .createQueryBuilder('skill')
+      .where({ id: skill.id })
+      .getOne();
 
     if (!skillExists) {
       throw new NotFoundException('Skill not found :(');
     }
 
-    await this.skillRepository.update(skill.id, skill);
-    return await this.skillRepository.findOneBy({ id: skill.id });
+    await this.skillRepository
+      .createQueryBuilder('skill')
+      .update(SkillEntity)
+      .set({ name: skill.name })
+      .where({ id: skill.id })
+      .execute();
+
+    return await this.skillRepository
+      .createQueryBuilder('skill')
+      .where({ id: skill.id })
+      .getOne();
   }
 
-  public async delete(id: number): Promise<Skill> {
-    const skillExists = await this.skillRepository.findOneBy({ id });
+  public async delete(id: number): Promise<SkillEntity> {
+    const skillExists = await this.skillRepository
+      .createQueryBuilder('skill')
+      .where({ id })
+      .getOne();
 
     if (!skillExists) {
       throw new NotFoundException('Skill not found :(');
