8a383f41d80a0b2e3aee8678e3257585ea9d14bb
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-25 17:15:57 -0600

    feat(skill.service): 🔨 Refactor the error handler with the ErrorManager

--- src/skill/services/skill.service.ts
+++ src/skill/services/skill.service.ts
@@ -1,13 +1,9 @@
-import {
-  ConflictException,
-  Injectable,
-  NotFoundException,
-  ServiceUnavailableException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SkillEntity } from '../entities';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { CreateSkillDto, UpdateSkillDto } from '../dto';
+import { ErrorManager } from 'src/utils/error.manager';
 
 @Injectable()
 export class SkillService {
@@ -17,63 +13,96 @@ export class SkillService {
   ) {}
 
   public async findAll(): Promise<SkillEntity[]> {
-    return await this.skillRepository.find();
+    try {
+      return await this.skillRepository.find();
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 
-  public async create(skill: CreateSkillDto): Promise<SkillEntity> {
-    const skillExists = await this.skillRepository.findOneBy({
-      name: skill.name,
-    });
+  public async create(body: CreateSkillDto): Promise<SkillEntity> {
+    try {
+      const skillExists = await this.skillRepository.findOneBy({
+        name: body.name,
+      });
 
-    if (skillExists) {
-      throw new ConflictException(`The skill ${skill.name} already exists`);
+      if (skillExists) {
+        throw new ErrorManager({
+          type: 'CONFLICT',
+          message: `The skill ${body.name} already exists`,
+        });
+      }
+
+      const newSkill = this.skillRepository.create(body);
+      return this.skillRepository.save(newSkill);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    const newSkill = this.skillRepository.create(skill);
-    return this.skillRepository.save(newSkill);
   }
 
-  public async update(skill: UpdateSkillDto): Promise<SkillEntity> {
-    const skillExists = await this.skillRepository
-      .createQueryBuilder('skill')
-      .where({ id: skill.id })
-      .getOne();
+  public async update(body: UpdateSkillDto): Promise<SkillEntity> {
+    try {
+      const skillExists = await this.skillRepository
+        .createQueryBuilder('skill')
+        .where({ id: body.id })
+        .getOne();
 
-    if (!skillExists) {
-      throw new NotFoundException('Skill not found :(');
+      if (!skillExists) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Skill not found',
+        });
+      }
+
+      const result: UpdateResult = await this.skillRepository
+        .createQueryBuilder('skill')
+        .update(SkillEntity)
+        .set({ name: body.name })
+        .where({ id: body.id })
+        .execute();
+
+      if (result.affected === 0) {
+        throw new ErrorManager({
+          type: 'NOT_MODIFIED',
+          message: 'Something went wrong, try it later',
+        });
+      }
+
+      return await this.skillRepository
+        .createQueryBuilder('skill')
+        .where({ id: body.id })
+        .getOne();
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    await this.skillRepository
-      .createQueryBuilder('skill')
-      .update(SkillEntity)
-      .set({ name: skill.name })
-      .where({ id: skill.id })
-      .execute();
-
-    return await this.skillRepository
-      .createQueryBuilder('skill')
-      .where({ id: skill.id })
-      .getOne();
   }
 
   public async delete(id: number): Promise<SkillEntity> {
-    const skillExists = await this.skillRepository
-      .createQueryBuilder('skill')
-      .where({ id })
-      .getOne();
+    try {
+      const skillExists = await this.skillRepository
+        .createQueryBuilder('skill')
+        .where({ id })
+        .getOne();
 
-    if (!skillExists) {
-      throw new NotFoundException('Skill not found :(');
+      if (!skillExists) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Skill not found :(',
+        });
+      }
+
+      const result: DeleteResult = await this.skillRepository.delete(id);
+
+      if (result.affected === 0) {
+        throw new ErrorManager({
+          type: 'NOT_MODIFIED',
+          message: 'Something went wrong, try it later',
+        });
+      }
+
+      return skillExists;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    const result = await this.skillRepository.delete(id);
-
-    if (result.affected === 0) {
-      throw new ServiceUnavailableException(
-        'Something went wrong, try it later',
-      );
-    }
-
-    return skillExists;
   }
 }
