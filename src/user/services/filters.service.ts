ce7454ac2a260fd30cc92d9963403523a930bd50
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-25 17:15:12 -0600

    feat(user.service): 🔨 Refactor the error handler with the ErrorManager

--- src/user/services/filters.service.ts
+++ src/user/services/filters.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import { ErrorManager } from 'src/utils/error.manager';
 
 @Injectable()
 export class UserFiltersService {
@@ -11,58 +12,70 @@ export class UserFiltersService {
   ) {}
 
   public async findAllAddProfile(): Promise<UserEntity[]> {
-    return await this.userRepository.find({
-      //* select: ['email'],
-      // De esta forma se pueden establecer que campos se
-      // quieren obtener en la consulta
-      //* where: {
-      //*   accountBill: Like(`${accountBill}%`),
-      //*   user: Not(`${user.id}`)
-      //* },
-      // De igual forma, podemos usar varios filtros dentro de la
-      // opcion where para filtrar aun mas nuestra busqueda
-      relations: ['profile'],
-      // Con relations es como establecemos la conexion con las tablas
-      // con las cuales tenemos relacion
-      select: {
-        profile: {
-          name: true,
-          profession: true,
+    try {
+      return await this.userRepository.find({
+        //* select: ['email'],
+        // De esta forma se pueden establecer que campos se
+        // quieren obtener en la consulta
+        //* where: {
+        //*   accountBill: Like(`${accountBill}%`),
+        //*   user: Not(`${user.id}`)
+        //* },
+        // De igual forma, podemos usar varios filtros dentro de la
+        // opcion where para filtrar aun mas nuestra busqueda
+        relations: ['profile'],
+        // Con relations es como establecemos la conexion con las tablas
+        // con las cuales tenemos relacion
+        select: {
+          profile: {
+            name: true,
+            profession: true,
+          },
         },
-      },
-      // Tambien, a traves de un select, podemos acceder a la propiedad
-      // de la relacion y especificar que campos de la relacion queremos
-      // que sean devueltos
-    });
+        // Tambien, a traves de un select, podemos acceder a la propiedad
+        // de la relacion y especificar que campos de la relacion queremos
+        // que sean devueltos
+      });
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 
   public async findAllAddOccupations(): Promise<UserEntity[]> {
-    return await this.userRepository.find({
-      relations: ['occupations'],
-      select: {
-        occupations: {
-          name: true,
-          yearsExperience: true,
-          monthsExperience: true,
+    try {
+      return await this.userRepository.find({
+        relations: ['occupations'],
+        select: {
+          occupations: {
+            name: true,
+            yearsExperience: true,
+            monthsExperience: true,
+          },
         },
-      },
-    });
+      });
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 
   public async findAllAddRelations(): Promise<UserEntity[]> {
-    return await this.userRepository.find({
-      relations: ['profile', 'occupations'],
-      select: {
-        profile: {
-          name: true,
-          profession: true,
+    try {
+      return await this.userRepository.find({
+        relations: ['profile', 'occupations'],
+        select: {
+          profile: {
+            name: true,
+            profession: true,
+          },
+          occupations: {
+            name: true,
+            yearsExperience: true,
+            monthsExperience: true,
+          },
         },
-        occupations: {
-          name: true,
-          yearsExperience: true,
-          monthsExperience: true,
-        },
-      },
-    });
+      });
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 }
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,14 +1,10 @@
-import {
-  Injectable,
-  ConflictException,
-  NotFoundException,
-  ServiceUnavailableException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserEntity } from '../entities/user.entity';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { ErrorManager } from 'src/utils/error.manager';
 
 @Injectable()
 export class UserService {
@@ -17,73 +13,128 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  public async exists(email: string): Promise<boolean> {
-    const userFound = await this.userRepository.findOneBy({ email });
+  public async create(body: CreateUserDto): Promise<UserEntity> {
+    try {
+      const userExists = await this.userRepository.findOneBy({
+        email: body.email,
+      });
 
-    return userFound ? true : false;
-  }
+      if (userExists) {
+        throw new ErrorManager({
+          type: 'CONFLICT',
+          message: `The email ${body.email} is already in use`,
+        });
+      }
 
-  public async create(user: CreateUserDto): Promise<UserEntity> {
-    const userExists = await this.exists(user.email);
-
-    if (userExists) {
-      throw new ConflictException(`The e-mail ${user.email} is already in use`);
+      const newUser = this.userRepository.create(body);
+      return await this.userRepository.save(newUser);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    const userCreated = this.userRepository.create(user);
-    return await this.userRepository.save(userCreated);
   }
 
   public async save(user: CreateUserDto): Promise<UserEntity> {
-    return await this.userRepository.save(user);
+    try {
+      return await this.userRepository.save(user);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 
   public async findAll(): Promise<UserEntity[]> {
-    return this.userRepository.find();
+    try {
+      return this.userRepository.find();
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 
   public async findOne(id: string): Promise<UserEntity> {
-    const userFound = await this.userRepository
-      .createQueryBuilder('user')
-      .where({ id })
-      .getOne();
+    try {
+      const userFound = await this.userRepository
+        .createQueryBuilder('user')
+        .where({ id })
+        .getOne();
 
-    if (!userFound) {
-      throw new NotFoundException('User not found');
+      if (!userFound) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'User not found :(',
+        });
+      }
+
+      return userFound;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    return userFound;
   }
 
   public async findByEmail(email: string): Promise<UserEntity | undefined> {
-    return this.userRepository.findOneBy({ email });
+    try {
+      return this.userRepository.findOneBy({ email });
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 
   public async update(id: string, user: UpdateUserDto): Promise<UserEntity> {
-    await this.findOne(id);
+    try {
+      const userExists = await this.userRepository
+        .createQueryBuilder('user')
+        .where({ id })
+        .getOne();
 
-    const result = await this.userRepository.update(id, user);
+      if (!userExists) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'User not found :(',
+        });
+      }
 
-    if (result.affected === 0) {
-      throw new ServiceUnavailableException(
-        'Something went wrong, try it later',
-      );
+      const result = await this.userRepository.update(id, user);
+
+      if (result.affected === 0) {
+        throw new ErrorManager({
+          type: 'NOT_MODIFIED',
+          message: 'Something went wrong, try it later',
+        });
+      }
+
+      return this.userRepository
+        .createQueryBuilder('user')
+        .where({ id })
+        .getOne();
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    return this.findOne(id);
   }
 
   public async deleteOne(id: string): Promise<UserEntity> {
-    const userFound = await this.findOne(id);
+    try {
+      const userFound = await this.userRepository
+        .createQueryBuilder('user')
+        .where({ id })
+        .getOne();
 
-    const result = await this.userRepository.delete(id);
+      if (!userFound) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'User not found : (',
+        });
+      }
 
-    if (result.affected === 0) {
-      throw new ServiceUnavailableException(
-        'Something went wrong, try it later',
-      );
+      const result = await this.userRepository.delete(id);
+
+      if (result.affected === 0) {
+        throw new ErrorManager({
+          type: 'NOT_MODIFIED',
+          message: 'Something went wrong, try it later',
+        });
+      }
+
+      return userFound;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    return userFound;
   }
 }
