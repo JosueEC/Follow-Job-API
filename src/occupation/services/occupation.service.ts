ededf63cb1afd99fcffa26d40bd44feb8616e8cd
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-25 17:15:27 -0600

    feat(occupation.service): 🔨 Refactor the error handler with the ErrorManager

--- src/occupation/services/occupation.service.ts
+++ src/occupation/services/occupation.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OccupationEntity } from '../entities/occupation.entity';
 import { Repository } from 'typeorm';
 import { CreateOccupationDto } from '../dto/create-occupation.dto';
 import { UserService } from '../../user/services/user.service';
+import { ErrorManager } from 'src/utils/error.manager';
 
 @Injectable()
 export class OccupationService {
@@ -13,22 +14,29 @@ export class OccupationService {
     private readonly userService: UserService,
   ) {}
 
-  public async create(
-    occupationDto: CreateOccupationDto,
-  ): Promise<OccupationEntity> {
-    const userFound = await this.userService.findOne(
-      occupationDto.professionalId,
-    );
+  public async create(body: CreateOccupationDto): Promise<OccupationEntity> {
+    try {
+      const userFound = await this.userService.findOne(body.professionalId);
 
-    if (!userFound) {
-      throw new NotFoundException('User not found :(');
+      if (!userFound) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'User not found :(',
+        });
+      }
+
+      const newOccupation = this.occupationRepository.create(body);
+      return await this.occupationRepository.save(newOccupation);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
     }
-
-    const newOccupation = this.occupationRepository.create(occupationDto);
-    return await this.occupationRepository.save(newOccupation);
   }
 
   public async findAll(): Promise<OccupationEntity[]> {
-    return await this.occupationRepository.find();
+    try {
+      return await this.occupationRepository.find();
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
   }
 }
