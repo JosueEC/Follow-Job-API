99ebab0ed231ee09bfc52dcf8e92f33f43bae5bd
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-04 13:30:17 -0600

    feat(company.entity): 🔨 Add the @OneToMany relationship between the  CompanyEntity and the NetworkEntity. Also add the 'cascade: true' and 'eager: true' property to the relation

--- src/company/entities/company.entity.ts
+++ src/company/entities/company.entity.ts
@@ -2,6 +2,7 @@ import { BaseEntity } from '../../config/base.entity';
 import { ICompany } from '../interfaces/company.interface';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { VacancyEntity } from '../../vacancy/entities/vacancy.entity';
+import { NetworkEntity } from '../../network/entities/network.entity';
 
 @Entity({ name: 'company' })
 export class CompanyEntity extends BaseEntity implements ICompany {
@@ -17,4 +18,10 @@ export class CompanyEntity extends BaseEntity implements ICompany {
     cascade: true,
   })
   vacancies: VacancyEntity[];
+
+  @OneToMany(() => NetworkEntity, (network) => network.company, {
+    cascade: true,
+    eager: true,
+  })
+  networks: NetworkEntity[];
 }
