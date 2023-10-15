6ce295cd06304d2d8094f25c4b76d5a251f1f37b
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-15 15:36:41 -0600

    refactor(networks.service): ♻️ Refactor the 'create' function with the following changes:
    - Use the 'findOne' function instead of the logic in the 'create' function to find a user
    - Add the 'bulkCreate' function to create many network entities
    - Replace the logic function for creating networks with the 'bulkCreate' function

--- src/network/services/network.service.ts
+++ src/network/services/network.service.ts
@@ -7,6 +7,7 @@ import { BulkCreateNetworkDto } from '../dto/bulk-create-network.dto';
 import { SocialNetwork } from '../enums/social-network.enum';
 import { ProfileService } from 'src/profile/services/profile.service';
 import { ProfileEntity } from 'src/profile/entities/profile.entity';
+import { CreateNetworkDto } from '../dto/create-network.dto';
 
 @Injectable()
 export class NetworkService {
@@ -18,7 +19,9 @@ export class NetworkService {
 
   public async create(body: BulkCreateNetworkDto): Promise<ProfileEntity> {
     try {
-      const profile = await this.profileService.findOne(body.profileId);
+      const profile: ProfileEntity = await this.profileService.findOne(
+        body.profileId,
+      );
 
       if (!profile) {
         throw new ErrorManager({
@@ -27,19 +30,25 @@ export class NetworkService {
         });
       }
 
-      const networksCreated: NetworkEntity[] = [...body.networks].map(
-        (network) => {
-          return this.networkRepository.create({
-            name: SocialNetwork[network.name],
-            url: network.url,
-          });
-        },
-      );
+      const networksCreated: NetworkEntity[] = this.bulkCreate(body.networks);
 
       profile.networks = networksCreated;
       await this.profileService.save(profile);
 
       return await this.profileService.findOne(profile.id);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public bulkCreate(networks: CreateNetworkDto[]): NetworkEntity[] {
+    try {
+      return [...networks].map((network) => {
+        return this.networkRepository.create({
+          name: SocialNetwork[network.name],
+          url: network.url,
+        });
+      });
     } catch (error) {
       throw ErrorManager.createSignatureError(error.message);
     }
