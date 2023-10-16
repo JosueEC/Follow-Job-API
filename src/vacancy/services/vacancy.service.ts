b4d576b25a16106d074e4b59b4d769b07db1dfb9
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-16 09:59:10 -0600

    feat(vacanyc.service): 🔨 Make the following changes:
    - Add the services for Company, Job, Location and Color
    - Change the 'create' function to an 'insert' function
    - Add the 'findOne' function to find a vacanyc by ID
    - Add the 'findOneWithRelations' function to find a vacancy with all its relations
    - Add the 'relatesVacancy' function to store a new vacancy with its relations

--- src/vacancy/services/vacancy.service.ts
+++ src/vacancy/services/vacancy.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InsertResult, Repository } from 'typeorm';
 import { VacancyEntity } from '../entities/vacancy.entity';
 import { UserEntity } from '../../user/entities/user.entity';
 import { ErrorManager } from '../../utils/error.manager';
@@ -8,6 +8,11 @@ import { UserService } from '../../user/services/user.service';
 import { UserFiltersService } from '../../user/services/filters.service';
 import { CreateVacancyDto } from '../dto/create-vacancy.dto';
 import { DeleteVacancyDto } from '../dto/delete-vacancy.dto';
+import { RelatesVacancyDto } from '../dto/relates-vacancy.dto';
+import { CompanyService } from 'src/company/services/company.service';
+import { JobService } from 'src/job/services/job.service';
+import { LocationService } from 'src/location/services/location.service';
+import { ColorSerivce } from 'src/color/services/color.service';
 
 @Injectable()
 export class VacancyService {
@@ -16,25 +21,86 @@ export class VacancyService {
     private readonly vacancyRepository: Repository<VacancyEntity>,
     private readonly userService: UserService,
     private readonly userFiltersService: UserFiltersService,
+    private readonly companyService: CompanyService,
+    private readonly jobService: JobService,
+    private readonly locationService: LocationService,
+    private readonly colorService: ColorSerivce,
   ) {}
 
-  public async create(
-    userId: string,
-    body: CreateVacancyDto,
-  ): Promise<VacancyEntity> {
+  public async insertOne(body: CreateVacancyDto): Promise<InsertResult> {
     try {
-      const user = await this.userService.findOneBasic(userId);
+      return await this.vacancyRepository.insert(body);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
 
-      if (!user) {
+  public async findOne(id: string): Promise<VacancyEntity> {
+    try {
+      const vacancy = await this.vacancyRepository
+        .createQueryBuilder('vacancy')
+        .where('vacancy.id = :vacancyId', { vacancyId: id })
+        .getOne();
+
+      if (!vacancy) {
         throw new ErrorManager({
           type: 'NOT_FOUND',
-          message: 'User not found :(',
+          message: 'Vacancy not found',
         });
       }
 
-      const newVacancy = this.vacancyRepository.create(body);
-      user.vacancies = [newVacancy];
+      return vacancy;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
 
+  public async findOneWithRelations(vacancyId: string): Promise<VacancyEntity> {
+    try {
+      const vacancy = await this.vacancyRepository
+        .createQueryBuilder('vacancy')
+        .leftJoin('vacancy.company', 'company')
+        .addSelect(['company.name'])
+        .leftJoin('vacancy.job', 'job')
+        .addSelect(['job.name'])
+        .leftJoin('vacancy.location', 'location')
+        .addSelect(['location.name'])
+        .leftJoin('vacancy.color', 'color')
+        .addSelect(['color.name'])
+        .where('vacancy.id = :vacancyId', { vacancyId })
+        .getOne();
+
+      if (!vacancy) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Vacancy not found',
+        });
+      }
+
+      return vacancy;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async relatesVacancy(body: RelatesVacancyDto): Promise<VacancyEntity> {
+    try {
+      const vacancy = await this.vacancyRepository.save(body.vacancy);
+      const user = await this.userFiltersService.findOneAddVacancies(
+        body.userId,
+      );
+      const company = await this.companyService.findOne(body.companyId);
+      const job = await this.jobService.findOne(body.jobId);
+      const location = await this.locationService.findOne(body.locationId);
+      const color = await this.colorService.findOne(body.colorId);
+
+      vacancy.company = company;
+      vacancy.job = job;
+      vacancy.location = location;
+      vacancy.color = color;
+
+      const newVacancy = await this.vacancyRepository.save(vacancy);
+      user.vacancies.push(newVacancy);
       await this.userService.save(user);
 
       return newVacancy;
@@ -43,7 +109,7 @@ export class VacancyService {
     }
   }
 
-  public async delete({
+  public async deleteOne({
     userId,
     vacancyId,
   }: DeleteVacancyDto): Promise<UserEntity> {
