4b7ea5902ca4639c0217b05113053e729e389ebd
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-16 10:06:30 -0600

    feat(vacancy.module): 🔨 Import the 'CompanyModule', 'JobModule', 'LocationModule' and 'ColorModule'

--- src/vacancy/vacancy.module.ts
+++ src/vacancy/vacancy.module.ts
@@ -3,9 +3,19 @@ import { VacancyController } from './controllers/vacancy.controller';
 import { VacancyService } from './services/vacancy.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VacancyEntity } from './entities/vacancy.entity';
+import { CompanyModule } from 'src/company/company.module';
+import { JobModule } from 'src/job/job.module';
+import { LocationModule } from 'src/location/location.module';
+import { ColorModule } from 'src/color/color.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VacancyEntity])],
+  imports: [
+    TypeOrmModule.forFeature([VacancyEntity]),
+    CompanyModule,
+    JobModule,
+    LocationModule,
+    ColorModule,
+  ],
   controllers: [VacancyController],
   providers: [VacancyService],
   exports: [],
