6a8335c4046c4a1c0e56482344f0db9389e67b79
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-16 10:09:52 -0600

    feat(location.controller): 🔨 Add the 'findOneLocation' function as a POST route to find a location with basic information

--- src/location/controllers/location.controller.ts
+++ src/location/controllers/location.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { LocationService } from '../services/location.service';
+import { CreateLocationDto } from '../dto/create-location.dto';
+import { InsertResult } from 'typeorm';
 
 @Controller('v1/location')
-export class LocationController {}
+export class LocationController {
+  constructor(private readonly locationService: LocationService) {}
+
+  @Post()
+  public async findOneLocation(
+    @Body() body: CreateLocationDto,
+  ): Promise<InsertResult> {
+    return this.locationService.insert(body);
+  }
+}
