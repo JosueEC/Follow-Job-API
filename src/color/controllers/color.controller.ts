6dd852245efe2d3468972a535780ded45c39eccb
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-16 10:26:57 -0600

    feat(color/dto): 🔨 Make the following changes:
    - In the color.controller, add the 'insertOneColor' function as a POST route that uses the 'insertOne' function in the ColorService
    - Export the ColorService and add the ColorEntity to the TypeOrmModule

--- src/color/controllers/color.controller.ts
+++ src/color/controllers/color.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { ColorSerivce } from '../services/color.service';
+import { InsertResult } from 'typeorm';
+import { CreateColorDto } from '../dto/create-color.dto';
 
 @Controller('v1/color')
-export class ColorController {}
+export class ColorController {
+  constructor(private readonly colorService: ColorSerivce) {}
+
+  @Post()
+  public async insertOneColor(
+    @Body() body: CreateColorDto,
+  ): Promise<InsertResult> {
+    return this.colorService.insertOne(body);
+  }
+}
