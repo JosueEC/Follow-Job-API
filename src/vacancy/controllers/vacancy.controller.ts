5bbb4928e5f1daf76f187fa59227071a0b315bbd
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-16 10:05:21 -0600

    feat(vacancy): 🔨 Add the following routes:
    - 'insertOneVacancy (POST)' to create a new vacancy in the database
    - 'relatesVacancy (POST)' to create a new vacancy with its relations
    - 'findOneVacancy' to find a vacancy with its relations

--- src/vacancy/controllers/vacancy.controller.ts
+++ src/vacancy/controllers/vacancy.controller.ts
@@ -1,26 +1,41 @@
-import { Controller, Body, Param, Post, Delete } from '@nestjs/common';
+import { Controller, Body, Param, Get, Post, Delete } from '@nestjs/common';
 import { VacancyService } from '../services/vacancy.service';
 import { CreateVacancyDto } from '../dto/create-vacancy.dto';
-import { VacancyEntity } from '../entities/vacancy.entity';
 import { UserEntity } from '../../user/entities/user.entity';
 import { DeleteVacancyDto } from '../dto/delete-vacancy.dto';
+import { InsertResult } from 'typeorm';
+import { RelatesVacancyDto } from '../dto/relates-vacancy.dto';
+import { VacancyEntity } from '../entities/vacancy.entity';
 
 @Controller('v1/vacancy')
 export class VacancyController {
   constructor(private readonly vacancyService: VacancyService) {}
 
-  @Post(':userId')
-  public async createOneVacancy(
-    @Param('userId') userId: string,
+  @Post()
+  public async insertOneVacancy(
     @Body() body: CreateVacancyDto,
+  ): Promise<InsertResult> {
+    return await this.vacancyService.insertOne(body);
+  }
+
+  @Post('/information')
+  public async relatesVacancy(
+    @Body() body: RelatesVacancyDto,
   ): Promise<VacancyEntity> {
-    return await this.vacancyService.create(userId, body);
+    return this.vacancyService.relatesVacancy(body);
+  }
+
+  @Get(':vacancyId')
+  public async findOneVacancy(
+    @Param('vacancyId') vacancyId: string,
+  ): Promise<VacancyEntity> {
+    return this.vacancyService.findOneWithRelations(vacancyId);
   }
 
   @Delete()
   public async deleteOneVacancy(
     @Body() body: DeleteVacancyDto,
   ): Promise<UserEntity> {
-    return await this.vacancyService.delete(body);
+    return await this.vacancyService.deleteOne(body);
   }
 }
