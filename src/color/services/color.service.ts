c75466e4298ef302e2fcb9a7cd77ae97d591d800
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-16 10:24:09 -0600

    feat(color.service): 🔨 Make the following changes:
    - Add the 'insertOne' function to store a new color in the database
    - Add the 'findOne' function to find a color by ID with its basic information

--- src/color/services/color.service.ts
+++ src/color/services/color.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ColorEntity } from '../entities/color.entity';
+import { InsertResult, Repository } from 'typeorm';
+import { ErrorManager } from 'src/utils/error.manager';
+import { CreateColorDto } from '../dto/create-color.dto';
 
 @Injectable()
-export class ColorSerivce {}
+export class ColorSerivce {
+  constructor(
+    @InjectRepository(ColorEntity)
+    private readonly colorRepository: Repository<ColorEntity>,
+  ) {}
+
+  public async insertOne(body: CreateColorDto): Promise<InsertResult> {
+    try {
+      return await this.colorRepository.insert(body);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async findOne(colorId: string): Promise<ColorEntity> {
+    try {
+      const color = await this.colorRepository
+        .createQueryBuilder('color')
+        .where('color.id = :colorId', { colorId })
+        .getOne();
+
+      if (!color) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Color not found',
+        });
+      }
+
+      return color;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+}
